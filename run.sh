4ebe69011f4a8bd500c97aebcfb4fdb853d13b1e
jis-u <yoo171084@gmail.com>
2021-12-03 20:57:47 +0900

    aug type 2 detail update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 ## pretrain
 
-for aug_t in 3 2;
+for aug_t in 1;
     do 
-    python main.py --learning_rate 1e-4 --weight_decay 1e-10 --model_type 'unet' --data_type 'both' --augmentation_type $aug_t
+    # python main.py --learning_rate 1e-4 --weight_decay 1e-10 --model_type 'unet' --data_type 'both' --augmentation_type $aug_t
     python main.py --learning_rate 1e-5 --weight_decay 1e-10 --model_type 'unet' --data_type 'both' --augmentation_type $aug_t
     python main.py --learning_rate 1e-6 --weight_decay 1e-10 --model_type 'unet' --data_type 'both' --augmentation_type $aug_t
 done
