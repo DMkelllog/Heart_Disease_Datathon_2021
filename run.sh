6bf6a8bd0527ca2a1040384ded8371acade0ed56
DMkelllog <ke1l1l1og@gmail.com>
2021-12-04 00:09:09 +0900

    fine tuning folder name

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 
 # Pretrain
-# python main.py --model_type 'caranet' --data_type 'both' --augmentation_type 3 --learning_rate 1e-3 --num_epochs=1
+python main.py --model_type 'caranet' --data_type 'both' --augmentation_type 3 --learning_rate 1e-3
 
 # Fine-tune (A2C) or (A4C)
 ## pretrained_path: pre_both_{aug_type}_{model}_{lr}
@@ -12,7 +12,7 @@ for d_type in 'A2C' 'A4C'
         do
         for epoch in 4 9 'full'
             do
-            python main.py --model_type 'caranet' --pretrained_path 'pre_both_3_caranet_0.001' --data_type $d_type --augmentation_type 3 --learning_rate $lr  --pretrained_epoch $epoch --memo $epoch --num_epochs=1
+            python main.py --model_type 'caranet' --pretrained_path 'pre_both_3_caranet_0.001' --data_type $d_type --augmentation_type 3 --learning_rate $lr  --pretrained_epoch $epoch --memo $epoch
         done
     done
 done
