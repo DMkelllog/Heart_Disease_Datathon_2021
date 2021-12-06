d74f548765aa00d4a9d082fb7a166d9dacf94f68
Hong-Jeongmin <hzeongmin@gmail.com>
2021-12-06 19:32:03 +0900

    DiceLoss

--- run.sh
+++ run.sh
@@ -2,17 +2,5 @@
 
 
 # Pretrain
-python main.py --model_type 'unet' --data_type 'both' --augmentation_type 3 --learning_rate 1e-3
-
-# Fine-tune (A2C) or (A4C)
-## pretrained_path: pre_both_{aug_type}_{model}_{lr}
-for d_type in 'A2C' 'A4C'
-    do
-    for lr in 1e-4 1e-5
-        do
-        for epoch in 4 9 'full'
-            do
-            python main.py --model_type 'unet' --pretrained_path 'pre_both_3_unet_0.001' --data_type $d_type --augmentation_type 3 --learning_rate $lr  --pretrained_epoch $epoch --memo $epoch
-        done
-    done
-done
+python main.py --model_type 'caranet' --data_type 'A2C' --augmentation_type 3 --learning_rate 1e-3 --pretrained_path "pre_both_1_caranet_0.001" --pretrained_epoch "full"
+python main.py --model_type 'caranet' --data_type 'A4C' --augmentation_type 3 --learning_rate 1e-3 --pretrained_path "pre_both_1_caranet_0.001" --pretrained_epoch "full"
