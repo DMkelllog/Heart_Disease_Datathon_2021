d87919ee63cbaddea19696162252559a1445f86f
Hong-Jeongmin <hzeongmin@gmail.com>
2021-12-06 19:32:38 +0900

    conflict

--- run.sh
+++
@@ -1,6 +0,0 @@
-## pretrain
-
-
-# Pretrain
-python main.py --model_type 'caranet' --data_type 'A2C' --augmentation_type 3 --learning_rate 1e-3 --pretrained_path "pre_both_1_caranet_0.001" --pretrained_epoch "full"
-python main.py --model_type 'caranet' --data_type 'A4C' --augmentation_type 3 --learning_rate 1e-3 --pretrained_path "pre_both_1_caranet_0.001" --pretrained_epoch "full"
